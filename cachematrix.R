f7d85fb64efaeca6df9f3184313e2d9d9ae4e78c
rj009 <ridhi.wrk@gmail.com>
2015-05-23 00:19:31 +0530

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix takes a matrix as an argument and returns a list of 4 functions when called. 
+## The 4 functions are
+##  1. Set : Set function sets up the data in the matrix and sets the inverted matrix to NULL.  
+##  2. Get : Get function simply returns the matrix to the calling environment. 
+##  3. Setinv : Sets the value of the inverted matrix to the incoming argument. 
+##  4. Getinv :  Returns the Inverted matrix value. 
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
 	
 	inv <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                inv <<- NULL
         }
         get <- function() {
                 x
@@ -21,24 +25,27 @@ makeCacheMatrix <- function(x = matrix()) {
         getinv <- function()    {
                 inv
         }
+
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function that returns the inverse of a square matrix using the solve function when called for the very first time. 
+## If cacheSolve is called on the same data again, the fuction forgoes calculations and instead 
+## pulls out the pre-existing data from the cache. 
 
-cacheSolve <- function(x, ...) {
+	cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-m <- x$getinv()
-        if(!is.null(m)) {
+	inv <- x$getinv()
+        if(!is.null(inv)) {
                 message("getting cached data")
-                return(m)
+                return(inv)
         }
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinv(m)
-        m
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 
 }
